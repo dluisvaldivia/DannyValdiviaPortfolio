410ad002cc138e1810ace755e521878fe7208042
Danny Valdivia <dluis.valdivia@gmail.com>
2025-03-30 21:18:17 +0000

    fixed navbar

--- src/view/components/Navbar.jsx
+++ src/view/components/Navbar.jsx
@@ -5,9 +5,8 @@ import ThemeToggle from "./ThemeToggle";
 export default function Navbar() {
   return (
     <nav>
-      <button className="item">Home</button>
       <button className="item">About</button>
-      <button className="item">Skills</button>
+      <button className="item">Projects</button>
       <button className="item">Contact</button>
       <ThemeToggle />
     </nav>
--- src/view/pages/Home.jsx
+++ src/view/pages/Home.jsx
@@ -4,38 +4,34 @@ export default function Home() {
   return (
 
     <div className='container'>
-      <div className='row text-center'>
-        <div className='col col-md-12 justify-align-center'>
+
+      <div className='row'>
+        <div className='col col-md-12 text-center'>
           <h1 className='heading1'>Danny Valdivia</h1>
           <h2>Full Stack Developer</h2>
-        </div>
-
-        <div className='row text-center'>
-          <h2>Technologies</h2>
-        </div>
-
-        <div className='col col-md-12'>
-          <div className='icon-block'>
-            <img className='tech-icons' src='src/assets/html-logo.png' alt='HTML5 icon' />
-            <span>HTML5</span>
-          </div>
-
-          <div className='icon-block'>
-            <img className='tech-icons' src='src/assets/css-logo.png' alt='CSS3 icon' />
-            <span>CSS3</span>
-          </div>
-
-          <div className='icon-block'>
-            <img className='tech-icons' src='src/assets/javascript-logo.png' alt='Javascript icon' />
-            <span>JavaScript</span>
-          </div>
-
         </div>
       </div>
 
       <div className='row text-start'>
-        <h2>My Bookmarks</h2>
+        <h2>About</h2>
+
       </div>
+
+      <div className='row text-start'>
+        <h2>Projects</h2>
+
+      </div>
+
+      <div className='row text-start'>
+        <h2>My Bookmarks</h2>
+
+      </div>
+
+      <div className='row text-start'>
+        <h2>Contact</h2>
+
+      </div>
+
     </div>
   );
 }
